7ee6e8fececdf0c9502e70b6ca3ac9f6074d888c
Matthew Dowdell <mdowdell244@gmail.com>
2013-09-02 00:28:58 +0100

    restructure, implement mw coding conventions

--- wikiScripts/headerLinks.js
+++ wikiScripts/headerLinks.js
@@ -8,83 +8,89 @@
  * having to dig it out of the ToC.
  *
  * @author  Cqm
- * @link    <http://dev.wikia.com/wiki/HeaderLinks>
- * @version 1.1
+ * @link    <http://dev.wikia.com/wiki/HeaderLinks> Documentation
+ * @version 1.1.2
  * @comment Does not work on file pages, due to different header tag structure
  */
 
 /*jshint
     browser:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
-    immed:true, indent:4, latedef:true, onevar:true, plusplus:true,
-    quotmark:single, strict:true, trailing:true, undef:true, unused:true
+    immed:true, indent:4, latedef:true, newcap:true, noarg:true,
+    noempty:true, nonew:true, onevar:true, plusplus:true, quotmark:single,
+    strict:true, trailing:true, undef:true, unused:true
 */
 
-;(function ($, mw, mwConfig) {
+;( function ( $, mw ) {
 
     'use strict';
 
-    var headerLinks = {
+    var translations = {
+        de: 'Link auf diesen Titel',
+        en: 'Link to this header',
+        nl: 'Link op deze titel',
+        pl: 'Link do nagłówka'
+    },
+    
+    headerLinks = {
         init: function () {
-
+            
+            var headers = $( '.mw-headline' );
+            
             // abort if no headers exist
-            if (!$('.mw-headline').length) {
+            if ( !headers.length ) {
                 return;
             }
-
+            
             // don't load twice
-            if ($('.mw-header-link').length) {
+            if ( $( '.mw-header-link' ).length ) {
                 return;
             }
-
+            
+            headerLinks.addLinks( headers );
+            
+        },
+        addLinks: function (headers) {
+            
             // append css to head
-            // easier that importing such a small amount of code
+            // easier than importing such a small amount of code
             mw.util.addCSS(
-                '.mw-header-link{float:right;opacity:0;transition:opacity 0.5s linear;}' +
+                '.mw-header-link{float:right;opacity:0;transition:opacity 0.3s linear;}' +
                 'h1:hover .mw-header-link,h2:hover .mw-header-link,h3:hover .mw-header-link,h4:hover .mw-header-link,h5:hover .mw-header-link,h6:hover .mw-header-link{opacity:0.5;}' +
                 '.mw-headline .mw-header-link:hover{opacity:1;}'
             );
-
-            headerLinks.addLinks();
-
-        },
-        addLinks: function () {
-            $('.mw-headline').each(function () {
-
-                var link = $('<span/>').attr({
-                        'class': 'mw-header-link',
-                        'title': headerLinks.i18n
-                    }).append(
-                        $('<a/>').attr({
-                            'href': '#' + $(this).attr('id')
-                        }).append(
-                            $('<img>').attr({
-                                'width': '20',
-                                'alt': 'Ambox padlock gray',
-                                'src': '//upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Ambox_padlock_gray.svg/20px-Ambox_padlock_gray.svg.png'
-                            })
-                        )
-                    );
-
-                $(this).append(link);
-
-            });
-        },
-        i18n: function () {
-
-            var translations = {
-                    en: 'Link to this header',
-                    pl: 'Link do nagłówka'
-                },
-                lang;
-
-            lang = translations[mwConfig.wgUserLanguage] || translations.en;
-            return lang;
-
+            
+            headers.each( function () {
+                
+                var $a = $( '<a/>' )
+                .attr( 'title',
+                    translations[ mw.config.get( 'wgUserLanguage' ) ] || translations.en
+                )
+                .append(
+                    $( '<img>' )
+                    .attr( {
+                        width: '20',
+                        alt: 'Ambox padlock gray',
+                        src: '//upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Ambox_padlock_gray.svg/20px-Ambox_padlock_gray.svg.png'
+                    } )
+                ),
+                
+                $span =  $( '<span/>' )
+                .attr( 'class', 'mw-header-link' )
+                .append( $a );
+                
+                return function () {
+                    var $h = $( this );
+                    $a.attr( 'href', '#' + $h.attr( 'id' ) );
+                    $h.append( $span.clone() );
+                };
+                
+            } () );
+            
         }
     };
 
-    $(headerLinks.init);
+    $( headerLinks.init );
+    
+}( this.jQuery, this.mediaWiki ) );
 
-}(this.jQuery, this.mediaWiki, this.mediaWiki.config.values));
-
-// </syntaxhighlight>
+// </syntaxhighlight> __NOWYSIWYG__
